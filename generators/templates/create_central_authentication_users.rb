fe871672d17889d65b058c7c9d9082557ce3a83d
Hannes Benson <hannes@mpowered.co.za>
2012-02-23 11:50:50 +0200

    fix the generator namespace issue

--- generators/templates/create_central_authentication_users.rb
+++ generators/templates/create_central_authentication_users.rb
@@ -1,9 +1,9 @@
 class CreateCentralAuthenticationUsers < ActiveRecord::Migration
   def self.up
     User.all.each do |user|
-      cu = CentralAuthentication::User.find_by_email user.email
+      cu = CentralAuthenticationRails2::User.find_by_email user.email
       if cu.nil?
-        cu = CentralAuthentication::User.create(:email => user.email, :password_salt => user.password_salt, :crypted_password => user.crypted_password, :perishable_token => user.perishable_token, :persistence_token => user.persistence_token)
+        cu = CentralAuthenticationRails2::User.create(:email => user.email, :password_salt => user.password_salt, :crypted_password => user.crypted_password, :perishable_token => user.perishable_token, :persistence_token => user.persistence_token)
         puts "Create user #{user.name}"
       else
         puts "Updating user #{user.name}"
