6a6cf020acb31fc70ed1a15efb5fe2de9533ede8
Hannes Benson <hannes@mpowered.co.za>
2012-02-23 12:30:36 +0200

    fix the double extensions that were being added to the migrations

--- generators/central_authentication_generator.rb
+++ generators/central_authentication_generator.rb
@@ -1,13 +1,13 @@
 class CentralAuthenticationGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.migration_template 'add_auth_id_to_users.rb', 'db/migrate', :migration_file_name => 'add_auth_id_to_users.rb'
+      m.migration_template 'add_auth_id_to_users', 'db/migrate', :migration_file_name => 'add_auth_id_to_users.rb'
       m.sleep(2)
-      m.migration_template 'add_users_table_to_cauth_db.rb', 'db/migrate', :migration_file_name => 'add_users_table_to_cauth_db.rb'
+      m.migration_template 'add_users_table_to_cauth_db', 'db/migrate', :migration_file_name => 'add_users_table_to_cauth_db.rb'
       m.sleep(2)
-      m.migration_template 'create_central_authentication_users.rb', 'db/migrate', :migration_file_name => 'create_central_authentication_users.rb'
+      m.migration_template 'create_central_authentication_users', 'db/migrate', :migration_file_name => 'create_central_authentication_users.rb'
       m.sleep(2)
-      m.migration_template 'remove_password_related_fields_from_user.rb', 'db/migrate', :migration_file_name => 'remove_password_related_fields_from_user.rb'
+      m.migration_template 'remove_password_related_fields_from_user', 'db/migrate', :migration_file_name => 'remove_password_related_fields_from_user.rb'
     end
   end
 end
